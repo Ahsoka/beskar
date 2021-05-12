8279c17145cf99af1961266099134d21b3ee677d
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-05-12 12:55:28 -0700

    Created write_2_5_volts function

--- beskar/gui.py
+++ beskar/gui.py
@@ -23,11 +23,12 @@ class BeskarWindow(QtWidgets.QWidget):
             self.device_name = system.devices.device_names[0]
             self.enter_volts_popup.show()
 
-        with nidaqmx.Task() as task:
-            task.ao_channels.add_ao_voltage_chan(f'{self.device_name}/ao0', min_val=0, max_val=5)
-            task.write(2.5) # Write 2.5 Volts
-
         # QtCore.QMetaObject.connectSlotsByName(self)
+
+    def write_2_5_volts(self):
+         with nidaqmx.Task() as task:
+                task.ao_channels.add_ao_voltage_chan(f'{self.device_name}/ao0', min_val=0, max_val=5)
+                task.write(2.5) # Write 2.5 Volts
 
     def closeEvent(self, close_event):
         self.closing = True
