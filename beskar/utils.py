55457f263ae145b1ac37b69c623b65a32bb0fe7b
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-05-12 13:21:12 -0700

    Use offset in apply_voltage function

--- beskar/utils.py
+++ beskar/utils.py
@@ -1,8 +1,9 @@
 from typing import Union
+from .constants import offset
 
 import nidaqmx
 
-def apply_voltage(device_name: str, voltage: Union[float, int] = 2.5, min_val: int = 0, max_val: int = 5):
+def apply_voltage(device_name: str, voltage: Union[float, int] = offset, min_val: int = 0, max_val: int = 5):
     with nidaqmx.Task() as task:
         task.ao_channels.add_ao_voltage_chan(
             f'{device_name}/ao0',
