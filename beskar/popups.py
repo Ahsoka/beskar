e2381e88b51350cdaf75d8969effc30c8880f9f6
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-05-11 17:19:53 -0700

    Created NoSEALKitPopup class in popups.py
    This popup will appear when there is no SEAL kit detected.  The popup will instruct the user to make sure the SEAL kit is connected properly and provide a refresh button to allow the user to check if the SEAL is detected.  Once the SEAL kit is detected they will be permitted to move onto the next portion of the application.

--- beskar/popups.py
+++ beskar/popups.py
@@ -1,4 +1,4 @@
-from PyQt6 import QtCore, QtWidgets
+from PyQt6 import QtCore, QtWidgets, QtTest
 from nidaqmx.system import System
 
 
@@ -40,5 +40,68 @@ class MultipleSEALKitsPopup(QtWidgets.QDialog):
             new_popup = MultipleSEALKitsPopup(self.parent)
             new_popup.label.setText(
                 "You must select which SEAL kit you want to use before starting the program."
+            )
+            new_popup.show()
+
+
+class NoSEALKitPopup(QtWidgets.QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.parent = parent
+
+        self.label = QtWidgets.QLabel(
+            "SEAL kit not detected, please make sure it's plugged in"
+        )
+        self.label.setObjectName('label')
+
+        self.combo_box = QtWidgets.QComboBox()
+        self.combo_box.setObjectName('combo_box')
+
+        self.push_button = QtWidgets.QPushButton('Refresh')
+        self.push_button.setObjectName('push_button')
+
+        self.vertical_layout = QtWidgets.QVBoxLayout()
+        self.vertical_layout.addWidget(self.label)
+        self.vertical_layout.addWidget(self.push_button, 0, QtCore.Qt.Alignment.AlignRight)
+
+        self.setLayout(self.vertical_layout)
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    @QtCore.pyqtSlot()
+    def on_push_button_clicked(self):
+        if self.push_button.text() == 'Refresh':
+            self.label.setText('Looking')
+            for iterr in range(5):
+                QtTest.QTest.qWait(100)
+                self.label.setText(f"{self.label.text()}.")
+            system = System.local()
+            if len(system.devices) == 0:
+                self.label.setText('SEAL kit not detected please try refreshing again')
+            else:
+                for device_name in system.devices.device_names:
+                    self.combo_box.addItem(device_name)
+                if len(system.devices.device_names) == 1:
+                    self.label.setText("The SEAL kit has been detected! Click 'OK' to access the program.")
+                else:
+                    self.label.setText(
+                        "Several SEAL kits have been detected. Please select which one you would like to use and click 'OK'."
+                    )
+                self.vertical_layout.insertWidget(1, self.combo_box)
+                self.push_button.setText('OK')
+        elif self.push_button.text() == 'OK':
+            self.parent.device_name = self.combo_box.currentText()
+            self.accept()
+            self.parent.enter_volts_popup.show()
+        else:
+            raise RuntimeError('This should never be triggered.')
+
+    def closeEvent(self, close_event):
+        if not self.parent.closing:
+            new_popup = NoSEALKitPopup(self.parent)
+            new_popup.label.setText(
+                "You cannot use the progam until the SEAL kit is detected. "
+                "Please refresh until it is found."
             )
             new_popup.show()
