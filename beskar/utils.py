25bb80d4f987bda201da82e525530cd3dc7f3708
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-05-21 11:42:53 -0700

    Removed min_val and max_val arguments from apply_voltage function
    These values are part of the problem in #1.  These values are constant and cannot be changed from 0 and 5, respectively.

--- beskar/utils.py
+++ beskar/utils.py
@@ -3,11 +3,7 @@ from .constants import offset
 
 import nidaqmx
 
-def apply_voltage(device_name: str, voltage: Union[float, int] = offset, min_val: int = 0, max_val: int = 5):
+def apply_voltage(device_name: str, voltage: Union[float, int] = offset):
     with nidaqmx.Task() as task:
-        task.ao_channels.add_ao_voltage_chan(
-            f'{device_name}/ao0',
-            min_val=min_val,
-            max_val=max_val
-        )
+        task.ao_channels.add_ao_voltage_chan(f'{device_name}/ao0', min_val=0, max_val=5)
         task.write(voltage)
