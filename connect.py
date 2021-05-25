7a67f17198656937ed3c6ccb3b83281023759b7b
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-05-25 15:15:46 -0700

    Deleted connect.py

--- connect.py
+++
@@ -1,8 +0,0 @@
-import nidaqmx.system
-system = nidaqmx.system.System.local()
-device = system.devices['Dev1']
-print(device)
-print(device.product_type)
-print(device.terminals)
-print(device.ao_voltage_rngs)
-print(device.ai_voltage_rngs)
