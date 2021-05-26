38048b5e372d7313d42f73f2e5e8e5f6a5e540a2
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-05-26 11:21:33 -0700

    Created LED_position_gen generator in utils.py

--- beskar/utils.py
+++ beskar/utils.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Generator, Tuple, Union
 from .constants import offset
 
 import nidaqmx
@@ -7,3 +7,23 @@ def apply_voltage(device_name: str, voltage: Union[float, int] = offset):
     with nidaqmx.Task() as task:
         task.ao_channels.add_ao_voltage_chan(f'{device_name}/ao0', min_val=0, max_val=5)
         task.write(voltage)
+
+def LED_position_gen(start_at_zero=False) -> Generator[Tuple[int, int], None, None]:
+    offset = 1 if start_at_zero else 0
+
+    row_pattern = [7, 2, 5, 4, 3, 6, 1, 8]
+    column_pattern = [8, 2, 6, 7, 3, 5, 1]
+
+    yield (2 - offset, 4 - offset)
+    yield (2 - offset, 4 - offset)
+    for row in row_pattern[2:]:
+        yield (row - offset, 4 - offset)
+    for column in column_pattern:
+        for row in row_pattern:
+            yield (row - offset, column - offset)
+
+    while True:
+        yield (7 - offset, 4 - offset)
+        for column in [4] + column_pattern:
+            for row in row_pattern:
+                yield (row - offset, column - offset)
