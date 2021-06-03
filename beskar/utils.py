b363f3a33741cb56184c3cae539d8d960e80658d
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-06-03 16:23:11 -0700

    Minor change to the first tuple yielded in LED_position_gen
    This change was made to more easily indicate whether the first cycle or the second cycle was occurring.

--- beskar/utils.py
+++ beskar/utils.py
@@ -10,13 +10,13 @@ def apply_voltage(device_name: str, voltage: Union[float, int] = offset):
         task.ao_channels.add_ao_voltage_chan(f'{device_name}/ao0', min_val=0, max_val=5)
         task.write(voltage)
 
-def LED_position_gen(start_at_zero=False) -> Generator[Tuple[int, int], None, None]:
+def LED_position_gen(start_at_zero=False) -> Generator[Union[Tuple[int, int], Tuple[int, int, None]], None, None]:
     offset = 1 if start_at_zero else 0
 
     row_pattern = [7, 2, 5, 4, 3, 6, 1, 8]
     column_pattern = [8, 2, 6, 7, 3, 5, 1]
 
-    yield (2 - offset, 4 - offset)
+    yield (2 - offset, 4 - offset, None)
     yield (2 - offset, 4 - offset)
     for row in row_pattern[2:]:
         yield (row - offset, 4 - offset)
