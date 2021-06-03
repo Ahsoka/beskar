ec8797d60393b0451d662ad20f889406818f4e64
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-06-03 16:24:06 -0700

    Added typing information to the start_at_zero parameter

--- beskar/utils.py
+++ beskar/utils.py
@@ -10,7 +10,7 @@ def apply_voltage(device_name: str, voltage: Union[float, int] = offset):
         task.ao_channels.add_ao_voltage_chan(f'{device_name}/ao0', min_val=0, max_val=5)
         task.write(voltage)
 
-def LED_position_gen(start_at_zero=False) -> Generator[Union[Tuple[int, int], Tuple[int, int, None]], None, None]:
+def LED_position_gen(start_at_zero: bool = False) -> Generator[Union[Tuple[int, int], Tuple[int, int, None]], None, None]:
     offset = 1 if start_at_zero else 0
 
     row_pattern = [7, 2, 5, 4, 3, 6, 1, 8]
