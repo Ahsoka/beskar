4ea9cb300a69848f63d18e30e4853a9d69e803f7
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-06-04 00:30:33 -0700

    Uploaded convert-to-exe.bat
    This is used for converting the Python files into an exe with necessary DLLs, pyd files, etc

---
+++ convert-to-exe.bat
@@ -0,0 +1,10 @@
+pyinstaller --name "Beskar" ^
+    --clean ^
+    --icon "beskar-icon.ico" ^
+    --paths "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\bin" ^
+    --paths "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\platforms" ^
+    --hidden-import "PyQt6.sip" --hidden-import "PyQt6.QtPrintSupport" ^
+    --add-data "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\platforms;platforms" ^
+    --add-data "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\styles;styles"  run.py
+
+REM Reference: https://stackoverflow.com/questions/66286229/create-pyqt6-python-project-executable
