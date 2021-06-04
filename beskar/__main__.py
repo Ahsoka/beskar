4b46b205dd7d44c884b5e9c1d3e050b82f044ae5
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-06-04 12:44:24 -0700

    Deleted old comments from __main__.py

--- beskar/__main__.py
+++ beskar/__main__.py
@@ -1,12 +1,10 @@
 from PyQt6.QtWidgets import QApplication
 from .gui import BeskarWindow
-# from .side_bar import Window
 
 def main():
     import sys
 
     app = QApplication(sys.argv)
-    # window = Window()
     window = BeskarWindow()
     window.show()
     sys.exit(app.exec())
