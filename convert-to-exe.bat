76096a7db54038223a90dc4e3295fde9d7d05246
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-06-30 21:22:30 -0700

    Add images folder into executable folder

--- convert-to-exe.bat
+++ convert-to-exe.bat
@@ -5,6 +5,7 @@ pyinstaller --name "Beskar" ^
     --paths "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\bin" ^
     --paths "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\platforms" ^
     --hidden-import "PyQt6.sip" --hidden-import "PyQt6.QtPrintSupport" ^
+    --add-data "C:\Users\AA12 Louqe\Google Drive\Coding\Github Repositories\Beskar (Solar Army)\beskar\images;images" ^
     --add-data "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\platforms;platforms" ^
     --add-data "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\styles;styles"  run.py
 
