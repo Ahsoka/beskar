da6965a829e32ce8e17f11b4d16434f62f542eec
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-07-06 19:09:28 -0700

    Removed extra space in convert-to-exe.bat

--- convert-to-exe.bat
+++ convert-to-exe.bat
@@ -7,6 +7,6 @@ pyinstaller --name "Beskar" ^
     --hidden-import "PyQt6.sip" --hidden-import "PyQt6.QtPrintSupport" ^
     --add-data "C:\Users\AA12 Louqe\Google Drive\Coding\Github Repositories\Beskar (Solar Army)\beskar\images;images" ^
     --add-data "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\platforms;platforms" ^
-    --add-data "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\styles;styles"  run.py
+    --add-data "C:\ProgramData\Anaconda3\Lib\site-packages\PyQt6\Qt6\plugins\styles;styles" run.py
 
 REM Reference: https://stackoverflow.com/questions/66286229/create-pyqt6-python-project-executable
