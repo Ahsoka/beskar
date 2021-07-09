5efae9234e2ab376de8f80f06baae5a3c806ae22
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-07-08 22:23:16 -0700

    Replaced convert-to-exe.bat with convert-to-exe.py
    This makes it much easier for anyone other than myself to build the source code into binaries, since the paths are all dynamically created.

---
+++ convert-to-exe.py
@@ -0,0 +1,22 @@
+from PyInstaller.__main__ import run
+
+import PyQt6
+import pathlib
+
+pyqt6_dir = pathlib.Path(PyQt6.__file__).parent
+
+if __name__ == '__main__':
+    run([
+        'run.py',
+        '--name', 'Beskar',
+        '--clean',
+        '--noconfirm',
+        '--icon', r'beskar\images\beskar-icon.ico',
+        '--paths', f'{pyqt6_dir / "Qt6" / "bin"}',
+        f'--paths', f'{pyqt6_dir / "Qt6" / "plugins" / "platforms"}',
+        '--hidden-import', 'PyQt6.sip',
+        '--hidden-import', 'PyQt6.QtPrintSupport',
+        '--add-data', 'beskar\images;images',
+        '--add-data', f'{pyqt6_dir / "Qt6" / "plugins" / "platforms"};platforms',
+        '--add-data', f'{pyqt6_dir / "Qt6" / "plugins" / "styles"};styles'
+    ])
