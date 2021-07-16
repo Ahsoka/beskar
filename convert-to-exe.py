c8c1c92850eda769f60505ebf68bd878932bcb50
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-07-16 02:36:41 -0700

    Added cacert.pem file to build

--- convert-to-exe.py
+++ convert-to-exe.py
@@ -1,9 +1,11 @@
 from PyInstaller.__main__ import run
 
 import PyQt6
+import certifi
 import pathlib
 
 pyqt6_dir = pathlib.Path(PyQt6.__file__).parent
+certifi_dir = pathlib.Path(certifi.__file__).parent
 
 if __name__ == '__main__':
     run([
@@ -20,5 +22,6 @@ if __name__ == '__main__':
         '--add-data', r'beskar\images;images',
         '--add-data', f'{pyqt6_dir / "Qt6" / "plugins" / "platforms"};platforms',
         '--add-data', f'{pyqt6_dir / "Qt6" / "plugins" / "styles"};styles',
-        '--add-data', r'beskar\desc;desc'
+        '--add-data', r'beskar\desc;desc',
+        '--add-data', f'{certifi_dir / "cacert.pem"};certifi'
     ])
