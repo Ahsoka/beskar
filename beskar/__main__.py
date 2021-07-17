6d97cf5b063c036109cc5efd781242c2ca983433
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-07-16 17:10:24 -0700

    Fixed issue where logging was not found when app is frozen
    Made logging in this function more robust

--- beskar/__main__.py
+++ beskar/__main__.py
@@ -1,4 +1,5 @@
 from sentry_sdk.utils import event_from_exception
+from PyQt6.QtCore import QCoreApplication
 from .settings import logging_dir
 from .popups import ErrorPopup
 from .logs import setUpLogger
@@ -6,6 +7,7 @@ from sentry_sdk import Hub
 from . import app, window
 
 import sys
+import pathlib
 import logging
 import inspect
 import traceback
@@ -18,22 +20,39 @@ def handle_exception(exc_type, exc_value, trace):
         popup = ErrorPopup(window, error_message)
         popup.exec()
 
-        # Get logger for the file that is responsible for error
-        logger = logging.getLogger(inspect.getmodule(inspect.stack()[1].frame).__name__)
-        logger.critical('The following error occured:', exc_info=exc_tuple)
-
         if popup.sending:
             hub = Hub.current
             if hub.client is not None:
                 hub.capture_event(*event_from_exception(exc_tuple))
                 hub.flush()
+
+        stack = inspect.stack()[1]
+        module = inspect.getmodule(stack.frame)
+        if module:
+            module_name = module.__name__
+        else:
+            path = pathlib.Path(stack.filename)
+            module_name = f"{path.parent.name}.{path.stem}"
+
+        if module_name in sys.modules:
+            logger = logging.getLogger(module_name)
+        else:
+            logger = logging.getLogger('beskar.unknown')
+            logger.warning(
+                'Failed to determine which module caused '
+                'the error in the upcoming logging message.'
+            )
+
+        logger.critical('The following error occured:', exc_info=exc_tuple)
+
+        QCoreApplication.quit()
     finally:
         sys.exit(1)
 
 def main():
     for logger_name in map(
         lambda name: f'beskar.{name}',
-        ('gui', 'pages', 'popups', 'settings', 'utils')
+        ('gui', 'pages', 'popups', 'settings', 'utils', 'unknown')
     ):
         setUpLogger(
             logger_name,
