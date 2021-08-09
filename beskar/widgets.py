3bc7c7df1faeeddb55b5e88440558cb79ca322e0
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-08-08 20:14:38 -0700

    Created LinkHoverColorChange widget
    This widget allows the color of a link to change when the user hovers over it.  See https://stackoverflow.com/questions/68690207/how-do-i-change-the-color-of-a-link-temporarily-when-hovered-in-a-qlabel

--- beskar/widgets.py
+++ beskar/widgets.py
@@ -1,1 +1,30 @@
+from PyQt6 import QtWidgets, QtCore, QtGui
 
+
+class LinkHoverColorChange(QtWidgets.QLabel):
+    def __init__(self, link_color1: str, link_color2: str, *args, **kwargs):
+        self.link_color1 = link_color1
+        self.link_color2 = link_color2
+
+        self.unformated_text = ''
+        if args and isinstance(args[0], str):
+            self.unformated_text = args[0]
+
+        super().__init__(*args, **kwargs)
+
+        self.linkHovered.connect(self.change_link_color)
+
+    def setText(self, text: str) -> None:
+        self.unformated_text = text
+        super().setText(text.format(self.link_color1))
+
+    def _set_formatted_text(self, text: str):
+        super().setText(text)
+
+    def change_link_color(self, link):
+        if link:
+            self._set_formatted_text(self.unformated_text.format(self.link_color2))
+            self.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        else:
+            self._set_formatted_text(self.unformated_text.format(self.link_color1))
+            self.unsetCursor()
