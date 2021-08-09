f7befcbd175404afbdcd0a0f38e8cf47db819623
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-08-08 20:31:34 -0700

    Created StepProgressBar widget
    This widget allows the user to see the total number of steps left to complete and the total number of steps they have completed already.

--- beskar/widgets.py
+++ beskar/widgets.py
@@ -1,4 +1,8 @@
+from .utils import get_file, get_folder, sort_frames
 from PyQt6 import QtWidgets, QtCore, QtGui, QtSvg
+from typing import Literal, Union
+
+import itertools
 
 
 class LinkHoverColorChange(QtWidgets.QLabel):
@@ -45,3 +49,168 @@ class LabelWithIcon(LinkHoverColorChange):
             QtCore.QRectF(3, 3, *(self.font().pixelSize(),) * 2)
         )
         super().paintEvent(paint_event)
+
+
+class StepProgressBar(QtWidgets.QWidget):
+    def __init__(
+        self,
+        steps: Literal[2, 3, 4],
+        *args,
+        checkmark_icon_dir_str: str = 'images/checkmark-frames',
+        **kwargs
+    ):
+        if steps not in {2, 3, 4}:
+            raise TypeError(
+                f"StepProgressBar only supports 2, 3, and 4 steps, not '{steps}'"
+            )
+
+        super().__init__(*args, **kwargs)
+
+        self.font_size = 8
+
+        self.inter_font = None
+        if font_loc := get_file('Inter-Light.ttf', 'fonts'):
+            inter_font_id = QtGui.QFontDatabase.addApplicationFont(font_loc)
+
+            inter_font_name = QtGui.QFontDatabase.applicationFontFamilies(
+                inter_font_id
+            )[0] or 'Inter Light'
+            self.inter_font = QtGui.QFontDatabase.font(
+                inter_font_name, '', self.font_size
+            )
+
+        checkmark_icon_dir = get_folder(checkmark_icon_dir_str)
+        self.checkmark_animated = bool(checkmark_icon_dir)
+        if self.checkmark_animated:
+            self.frames = sort_frames(checkmark_icon_dir, '*.svg')
+            self.counter = itertools.cycle(range(8))
+
+        self.checkmark_icon = get_file('checkmark-icon.svg')
+
+        self.total_steps = steps
+
+        self.current_step = 1
+
+        self.bubble_y = 15 + 1
+        self.bubble_height = 15
+        self.bubble_width = 15
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.repaint)
+
+        self.pen = QtGui.QPen(QtGui.QColorConstants.White, 1)
+
+        self.setSizePolicy(
+            QtWidgets.QSizePolicy.Policy.MinimumExpanding,
+            QtWidgets.QSizePolicy.Policy.Fixed
+        )
+
+    def sizeHint(self) -> QtCore.QSize:
+        return QtCore.QSize(100, self.bubble_height * 2 + 2)
+
+    def next_step(self):
+        self.timer.start(40)
+
+    def paintEvent(self, paint_event: QtGui.QPaintEvent):
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing, on=True)
+
+        if self.inter_font:
+            painter.setFont(self.inter_font)
+        painter.setPen(self.pen)
+
+        first_bubble_x = 30
+        last_bubble_x = paint_event.rect().width() - 30
+
+        self.draw_step_bubble(painter, first_bubble_x, 1)
+        self.draw_step_bubble(painter, last_bubble_x, self.total_steps)
+
+        if self.total_steps == 2:
+          second_bubble_x = last_bubble_x
+        elif self.total_steps == 3 or self.total_steps == 4:
+            if self.total_steps == 3:
+                second_bubble_x = paint_event.rect().width() / 2
+                third_bubble_x = last_bubble_x
+            elif self.total_steps == 4:
+                midpoint = paint_event.rect().width() - 60
+                second_bubble_x = round(midpoint / 3 + 30)
+                third_bubble_x = round(2 * midpoint / 3 + 30)
+
+                self.draw_step_bubble(painter, third_bubble_x, 3)
+
+                painter.drawLine(
+                    third_bubble_x + self.bubble_width,
+                    self.bubble_y,
+                    last_bubble_x - self.bubble_width,
+                    self.bubble_y
+                )
+
+            self.draw_step_bubble(painter, second_bubble_x, 2)
+
+            painter.drawLine(
+                second_bubble_x + self.bubble_width,
+                self.bubble_y,
+                third_bubble_x - self.bubble_width,
+                self.bubble_y
+            )
+
+        painter.drawLine(
+            first_bubble_x + self.bubble_width,
+            self.bubble_y,
+            second_bubble_x - self.bubble_width,
+            self.bubble_y
+        )
+
+    def draw_step_bubble(
+        self,
+        painter: QtGui.QPainter,
+        center_x: int,
+        number: Union[str, int],
+        center_y: int = None,
+    ):
+        svg_renderer = None
+
+        if center_y is None:
+            center_y = self.bubble_y
+
+        if isinstance(number, str):
+            number = int(number)
+
+        painter.drawEllipse(
+            QtCore.QPoint(center_x, center_y),
+            self.bubble_width,
+            self.bubble_height
+        )
+
+        if number < self.current_step and self.checkmark_icon:
+            svg_renderer = QtSvg.QSvgRenderer(self.checkmark_icon)
+
+        if number == self.current_step and self.checkmark_animated and self.timer.isActive():
+            frame, counter  = next(self.frames), next(self.counter)
+            svg_renderer = QtSvg.QSvgRenderer(str(frame))
+            if counter == 7:
+                self.timer.stop()
+                self.current_step += 1
+
+        if svg_renderer:
+            svg_renderer.setAspectRatioMode(QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+            svg_renderer.render(
+                painter,
+                QtCore.QRectF(
+                    # NOTE: Not sure if this pattern holds with variable font sizes.
+                    # Only tested with self.font_size=18
+                    center_x - self.bubble_width + self.font_size - 1,
+                    self.bubble_y - self.bubble_height + self.font_size - 1,
+                    self.font_size * 2,
+                    self.font_size * 2
+                )
+            )
+        else:
+            painter.drawText(
+                center_x - self.font_size / 2 + 1,
+                self.font_size / 2 + center_y,
+                str(number)
+            )
+
+        painter.setPen(self.pen)
