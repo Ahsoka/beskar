ddfd9cd0c88d14593cc756e5cfdaafbe092ee429
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-08-10 01:28:19 -0700

    Removed `AppleVoltagePage` from gui.py

--- beskar/gui.py
+++ beskar/gui.py
@@ -1,6 +1,6 @@
 from .popups import MultipleSEALKitsPopup, NoSEALKitPopup, EnterVoltsPopup
 from .utils import apply_voltage, get_file, get_number_of_devices
-from .pages import ApplyVoltagePage, DarkCurrentPage, ScanPage
+from .pages import DarkCurrentPage, ScanPage
 from PyQt6 import QtCore, QtWidgets, QtGui
 from . import __version__
 
@@ -32,14 +32,11 @@ class BeskarWindow(QtWidgets.QMainWindow):
 
         self.create_options_menu()
 
-        self.apply_voltage_widget = ApplyVoltagePage(self)
-
         self.dark_current_widget = DarkCurrentPage(self)
 
         self.scan_widget = ScanPage(self)
 
         self.stacked_widget = QtWidgets.QStackedWidget()
-        self.stacked_widget.addWidget(self.apply_voltage_widget)
         self.stacked_widget.addWidget(self.dark_current_widget)
         self.stacked_widget.addWidget(self.scan_widget)
 
@@ -87,11 +84,6 @@ class BeskarWindow(QtWidgets.QMainWindow):
         space_between_buttons = 20
 
         # TODO: Add description for each QCommandLinkButton
-        self.apply_voltage_menu = QtWidgets.QCommandLinkButton('Apply Voltage')
-        self.apply_voltage_menu.setObjectName('apply_voltage_menu_button')
-        self.apply_voltage_menu.setCheckable(True)
-        self.apply_voltage_menu.setChecked(True)
-
         spacer1 = QtWidgets.QSpacerItem(
             0, space_between_buttons, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Preferred
         )
@@ -113,7 +105,6 @@ class BeskarWindow(QtWidgets.QMainWindow):
         )
 
         self.vertical_menu_layout = QtWidgets.QVBoxLayout()
-        self.vertical_menu_layout.addWidget(self.apply_voltage_menu)
         self.vertical_menu_layout.addItem(spacer1)
         self.vertical_menu_layout.addWidget(self.dark_current_menu)
         self.vertical_menu_layout.addItem(spacer2)
@@ -128,20 +119,11 @@ class BeskarWindow(QtWidgets.QMainWindow):
         super().closeEvent(close_event)
 
     @QtCore.pyqtSlot()
-    def on_apply_voltage_menu_button_clicked(self):
-        self.apply_voltage_menu.setChecked(True)
-        self.dark_current_menu.setChecked(False)
+    def on_dark_current_menu_button_clicked(self):
+        self.dark_current_menu.setChecked(True)
         self.scan_menu.setChecked(False)
 
         self.stacked_widget.setCurrentIndex(0)
-
-    @QtCore.pyqtSlot()
-    def on_dark_current_menu_button_clicked(self):
-        self.dark_current_menu.setChecked(True)
-        self.apply_voltage_menu.setChecked(False)
-        self.scan_menu.setChecked(False)
-
-        self.stacked_widget.setCurrentIndex(1)
 
         self.dark_current_widget.update_data()
 
@@ -155,10 +137,9 @@ class BeskarWindow(QtWidgets.QMainWindow):
         self.dark_current_widget.update_data()
 
         self.scan_menu.setChecked(True)
-        self.apply_voltage_menu.setChecked(False)
         self.dark_current_menu.setChecked(False)
 
-        self.stacked_widget.setCurrentIndex(2)
+        self.stacked_widget.setCurrentIndex(1)
 
         self.high_dark_current(
             'Scanning may not be accurate due to unusually high dark current values. '
