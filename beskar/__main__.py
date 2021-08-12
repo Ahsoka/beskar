1956a4a4fe433097ea885245bf83d30274f8b188
Andres Torres <34612646+Ahsoka@users.noreply.github.com>
2021-08-11 17:44:08 -0700

    Update the `main` function

--- beskar/__main__.py
+++ beskar/__main__.py
@@ -1,7 +1,8 @@
 from .handle_errors import handle_exception
+from . import app, startup, update_thread
 from .settings import logging_dir
+from .gui import BeskarWindow
 from .logs import setUpLogger
-from . import app, window
 
 import sys
 
@@ -14,8 +15,15 @@ def main():
 
     sys.excepthook = handle_exception
 
-    window.show()
-    sys.exit(app.exec())
+    if update_thread:
+        update_thread.start()
+
+    startup.show()
+
+    if startup.exec():
+        window = BeskarWindow(startup.mocked, startup.device_name)
+        window.show()
+        sys.exit(app.exec())
 
 if __name__ == '__main__':
     main()
